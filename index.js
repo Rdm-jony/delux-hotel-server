7893d9592caae060a7029d26c0f11b667a7ad66c
Rdm-jony <jonydascse@gmail.com>
2022-11-07 21:06:26 -0800

    complete limited service

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const cors = require('cors');
-const app=express()
-const port=process.env.PORT||5000;
-
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const app = express()
+const port = process.env.PORT || 5000;
+require('dotenv').config()
 
 //middle Ware
 app.use(cors())
@@ -10,10 +11,32 @@ app.use(express.json())
 
 //mongodb connect
 
-app.get("/",(req,res)=>{
-res.send("review server is running")
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.tbsccmb.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        const serviceCollections = client.db("service-review").collection("services");
+        app.get("/services", async (req, res) => {
+            const query = {}
+            const cursor = serviceCollections.find(query);
+            const result = await cursor.limit(3).toArray()
+            res.send(result)
+        })
+    }
+    finally {
+
+    }
+}
+
+run().catch(er => console.log(er))
+
+
+
+app.get("/", (req, res) => {
+    res.send("review server is running")
 })
 
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log(`review server is running on port ${port}`)
 })
